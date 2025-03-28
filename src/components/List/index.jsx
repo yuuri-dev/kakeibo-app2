bcdb20c5a2fc53985f62e1b2d54c54730dfc00ac
YUURI Inoue <yuuri041125@gmail.com>
2025-03-28 16:07:48 +0900

    コンポーネントを作成

---
+++ src/components/List/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { params } from '../../pages/index';
+
+export default function List() {
+  const { is_Modal } = React.useContext(params);
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,33 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import Head from 'next/head';
+import Image from 'next/image';
+import { Geist, Geist_Mono } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import List from '@/components/List';
+import { useState, createContext } from 'react';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
+export const params = createContext();
+
 export default function Home() {
+  const [is_Modal, set_Modal] = useState(false);
+  const [sum, setSum] = useState(0);
+
   return (
     <>
-      <h1>家計簿アプリ</h1>
-      <h2>Kakeibo</h2>
+      <params.Provider value={(is_Modal, set_Modal)}>
+        <h1>家計簿アプリ</h1>
+        <h2>Kakeibo</h2>
+        <List />
+      </params.Provider>
     </>
   );
 }
