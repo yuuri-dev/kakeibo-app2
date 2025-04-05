e6721fbed22070bc03b8fdf2b4092737ed6ec027
YUURI Inoue <yuuri041125@gmail.com>
2025-04-05 12:03:30 +0900

    いらない部分の削除とcssの作成

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,9 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Geist, Geist_Mono } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import List from '@/components/List';
 import AddButton from '@/components/AddButton';
 import { useState, createContext } from 'react';
 import Modal from '@/components/Modal';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const Params_Context = createContext();
 
@@ -26,9 +14,11 @@ export default function Home() {
   return (
     <>
       <Params_Context.Provider value={{ is_Modal, set_Modal, sum, set_Sum }}>
-        <h1>家計簿アプリ</h1>
-        {is_Modal ? <List /> : <Modal /> }
-        <AddButton />
+        <div className={styles.container}>
+          <h1 className={styles.app_title}>家計簿アプリ</h1>
+          {is_Modal ? <List /> : <Modal />}
+          <AddButton />
+        </div>
       </Params_Context.Provider>
     </>
   );
